27bfe6995869a6dad672d41fd31c50969fd8c049
Max Patten <amanthus2535@gmail.com>
2019-05-21 17:59:43 -0700

    end of first extended session

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("IM WORKING!")
+console.log("IM WORKING!");
+
+// variables = nouns of programming
+// Numbers, String, Booleans 
+
+var beverage = "beer";
+var howMany = 6;
+var isThirsty = true; 
+
+alert("I'm currently not drinking a " + beverage + ".");
+alert("But over the week i'll drink " + howMany + " " + beverage + "s.");
+
+// if - controls the flow of logic in our program
+
+if(isThirsty) {
+    var askUser = confirm("Art you thirsty?");
+}
